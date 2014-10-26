75a198e45bc70dda35cd5c69d1b7afed71bb126e
Rajesh Devarajan <rajeshdevarajan@rajeshdsmacbook.home>
2014-10-26 08:55:09 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -6,7 +6,8 @@
 
 - (void)initialize
 {
-    // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
@@ -14,6 +15,9 @@
     // put update code here
 }
 
-// put new methods here
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    [character flap];
+}
 
 @end
